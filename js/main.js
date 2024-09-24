cbea143b5d797a498b2de2fbf7565b835950979c
maezoo <aa00456@naver.com>
2024-09-24 18:20:16 +0900

    prevent scroll in hamburger pop_up

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
-const ham_op = document.querySelector('.hamburger');
-const ham_cl = document.querySelector('.ham_close a');
-const ham_pop = document.querySelector('.hamburger_pop');
-
-ham_op.addEventListener('click', () => ham_pop.classList.toggle('show'));
-ham_cl.addEventListener('click', () => ham_pop.classList.toggle('show'));
-
-// ========================================================================================================
-
-const pop_btn = document.querySelector('.top_zone_btn');  // 버튼
-const pop = document.querySelector('.top_pop_up');     // 팝업 요소
-const btnElement = document.getElementById('btn_cl');  // 열기 버튼
+const pop_btn = document.querySelector('.top_zone_btn');
+const pop = document.querySelector('.top_pop_up');
+const btnElement = document.getElementById('btn_cl');
 
 function top_Popup() {
-    // 팝업 열기 버튼 클릭 시 팝업을 열거나 닫음
     pop_btn.addEventListener('click', function () {
         pop.classList.toggle('show');
     });
@@ -20,18 +10,35 @@ function top_Popup() {
 
 top_Popup();
 
-// // 팝업 닫기 버튼 클릭 시 팝업을 닫음
-// open_btn.addEventListener('click', function () {
-//     pop.classList.remove('show');
-// });
+// ========================================================================================================
+const ham_op = document.querySelector('.hamburger');
+const ham_cl = document.querySelector('.ham_close a');
+const ham_pop = document.querySelector('.hamburger_pop');
+const body = document.body;
 
+// 팝업 열기 이벤트
+ham_op.addEventListener('click', () => {
+    ham_pop.classList.toggle('show');
 
-//     document.getElementById('pop_up_top').style.display = "none";
-// }
-// 페이지 로드 시 팝업을 보이게 설정 (필요한 경우)
-// window.onload = function () {
-//     document.getElementById('pop_up_top').style.display = "block";
-// };
+    // 팝업이 열리면 no-scroll 클래스를 추가하여 스크롤을 비활성화
+    if (ham_pop.classList.contains('show')) {
+        body.classList.add('no-scroll');
+    } else {
+        body.classList.remove('no-scroll');
+    }
+});
+
+// 팝업 닫기 이벤트
+ham_cl.addEventListener('click', () => {
+    ham_pop.classList.toggle('show');
+
+    // 팝업이 닫히면 no-scroll 클래스를 제거하여 스크롤을 활성화
+    if (!ham_pop.classList.contains('show')) {
+        body.classList.remove('no-scroll');
+    } else {
+        body.classList.add('no-scroll');
+    }
+});
 
 // ========================================================================================================
 
