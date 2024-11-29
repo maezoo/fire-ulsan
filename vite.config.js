9d86bd34945eaeb1572557f034bfcedec3e740be
maezoo <aa00456@naver.com>
2024-11-29 16:09:05 +0900

    Fix style.scss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 글로벌 변수나 믹스인을 추가할 수 있습니다
+        // 예시: additionalData: `@use "src/styles/variables.scss" as *;`,
+      },
+    },
+  },
   base: '/portfolio-fire-ulsan/',
   build: {
     outDir: 'dist'
